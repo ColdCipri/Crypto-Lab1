cd43f7752bc50dc442255d8b5c47e024dc58756d
ColdCipri <colder.ciprian@gmail.com>
2019-11-07 14:09:20 +0200

    Updated project

--- Lab1 Cryptography/Form1.Designer.cs
+++ Lab1 Cryptography/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.label_input = new System.Windows.Forms.Label();
+            this.label_output = new System.Windows.Forms.Label();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.button_switch = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_input.Location = new System.Drawing.Point(15, 77);
+            this.textBox_input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(241, 117);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // label_input
+            // 
+            this.label_input.AutoSize = true;
+            this.label_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_input.Location = new System.Drawing.Point(6, 27);
+            this.label_input.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_input.Name = "label_input";
+            this.label_input.Size = new System.Drawing.Size(196, 46);
+            this.label_input.TabIndex = 1;
+            this.label_input.Text = "Plain text:";
+            // 
+            // label_output
+            // 
+            this.label_output.AutoSize = true;
+            this.label_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_output.Location = new System.Drawing.Point(343, 27);
+            this.label_output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_output.Name = "label_output";
+            this.label_output.Size = new System.Drawing.Size(287, 46);
+            this.label_output.TabIndex = 2;
+            this.label_output.Text = "Encrypted text:";
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Enabled = false;
+            this.textBox_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_output.Location = new System.Drawing.Point(352, 77);
+            this.textBox_output.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.Size = new System.Drawing.Size(281, 117);
+            this.textBox_output.TabIndex = 3;
+            // 
+            // button_switch
+            // 
+            this.button_switch.Location = new System.Drawing.Point(266, 125);
+            this.button_switch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_switch.Name = "button_switch";
+            this.button_switch.Size = new System.Drawing.Size(79, 28);
+            this.button_switch.TabIndex = 4;
+            this.button_switch.Text = "Switch";
+            this.button_switch.UseVisualStyleBackColor = true;
+            this.button_switch.Click += new System.EventHandler(this.button_switch_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_start.Location = new System.Drawing.Point(225, 241);
+            this.button_start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(164, 48);
+            this.button_start.TabIndex = 5;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(652, 304);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_switch);
+            this.Controls.Add(this.textBox_output);
+            this.Controls.Add(this.label_output);
+            this.Controls.Add(this.label_input);
+            this.Controls.Add(this.textBox_input);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Label label_input;
+        private System.Windows.Forms.Label label_output;
+        private System.Windows.Forms.TextBox textBox_output;
+        private System.Windows.Forms.Button button_switch;
+        private System.Windows.Forms.Button button_start;
     }
 }
 
--- Lab1 Cryptography/Form1.cs
+++ Lab1 Cryptography/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,98 @@ namespace Lab1_Cryptography
 {
     public partial class Form1 : Form
     {
+        ArrayList alphabet = new ArrayList() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
+        ArrayList randomList = new ArrayList(); 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private ArrayList substCypher(ArrayList alpha)
+        {
+            randomList = (ArrayList)alpha.Clone();
+
+
+            Random rand = new Random();
+            for (int i = 0; i < randomList.Capacity - 1; i++)
+            {
+                int j = rand.Next(i, randomList.Capacity);
+                var temp = randomList[i];
+                randomList[i] = randomList[j];
+                randomList[j] = temp;
+            }
+
+            return randomList;
+            
+        }
+
+        private void button_switch_Click(object sender, EventArgs e)
+        {
+            if (textBox_input.Enabled)
+            {
+                textBox_input.Enabled = false;
+                textBox_output.Enabled = true;
+
+                textBox_input.Text = textBox_output.Text = "";
+            }
+            else
+            {
+                textBox_input.Enabled = true;
+                textBox_output.Enabled = false;
+                
+                textBox_input.Text = textBox_output.Text = "";
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            if (randomList.Capacity == 0)
+            {
+                randomList = substCypher(alphabet);
+            }
+
+            if (textBox_input.Enabled)
+            {
+                var text = textBox_input.Text.ToString();
+                
+                var encrypted = encryptMethod(text);
+
+                textBox_output.Text = encrypted;
+            } else
+            {
+                var text = textBox_output.Text.ToString();
+
+                var decrypted = decryptMethod(text);
+
+                textBox_input.Text = decrypted;
+            }
+
+        }
+
+        private String encryptMethod(String text)
+        {
+            var encrypted = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var pos = alphabet.IndexOf(text[i]);
+                encrypted += randomList[pos].ToString();
+            }
+
+            return encrypted;
+        }
+
+        private String decryptMethod(String text)
+        {
+            var decrypted = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var pos = randomList.IndexOf(text[i]);
+                decrypted += alphabet[pos].ToString();
+            }
+
+            return decrypted;
         }
     }
 }
